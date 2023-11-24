f963a986e739c659703a9b8a50d9b90e90a5603b
maha <mahanandi.y@royalcyber.com>
2023-11-24 08:38:43 +0530

    Create EC2

--- EC2.sh
+++ EC2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web" )
 
-for in in "${NAMES[@]}"
+for i in "${NAMES[@]}"
 do
     echo "NAME: $i"
 done
