1aac8904d6f2d9ad83439227232527c5f488be1d
maha <mahanandi.y@royalcyber.com>
2023-11-24 09:12:39 +0530

    Create EC2

--- EC2.sh
+++ EC2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web" )
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-066e77f4c76020ae9
 
+# mysql or mongodb instance typr should br t3.medium
+
 for i in "${NAMES[@]}"
 do
-    if [ [ $i == "mongodb" || $i == "mysql"]];
+    if [[ $i == "mongodb" || $i == "mysql"]]
     then
         INSTANCE_TYPE="t3.medium"
     else
